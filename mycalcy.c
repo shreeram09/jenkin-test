a86ca1329d997c85952bf43c2a99e7eab281c488
shreeram a <auteshreeram@gmail.com>
2020-02-22 14:58:34 +0530

    my c calcy

---
+++ mycalcy.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+	float a=0, b=0;
+	printf("\nfirst number:");
+	scanf("%f",&a);
+	printf("\nSecond number:");
+	scanf("%f",&b);
+	printf("addition is %.2f",(a+b));
+}
