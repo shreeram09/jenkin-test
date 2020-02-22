f13e592443a535025a0fe52ffa1722bc89662e79
shreeram a <auteshreeram@gmail.com>
2020-02-22 16:17:16 +0530

    program code changed

--- mycalcy.c
+++ mycalcy.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
-void main(int* argc,char* argv[]){
+void main(int argc,char* argv[]){
 	float a=0, b=0;
-	if(*argc!=3){
+	if(argc!=3){
 		printf("please provide only 2 numbers to add.");
 		exit(1);
 	}
-	printf("\nfirst number:");
-	scanf("%f",&a);
-	printf("\nSecond number:");
-	scanf("%f",&b);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	printf("addition is %.2f",(a+b));
 }
