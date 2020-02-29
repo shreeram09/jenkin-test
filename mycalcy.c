8aee0e0f523afbe679f303546804711d6edfdd5b
shreeram a <auteshreeram@gmail.com>
2020-02-29 13:50:07 +0530

    newline added

--- mycalcy.c
+++ mycalcy.c
@@ -3,10 +3,14 @@
 void main(int argc,char* argv[]){
 	float a=0, b=0;
 	if(argc!=3){
+		printf("\n");
 		printf("please provide only 2 numbers to add.");
+		printf("\n");
 		exit(1);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
+	printf("\n");
 	printf("addition is %.2f",(a+b));
+	printf("\n");
 }
