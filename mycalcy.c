7f1d263e14aad55007950360e81cfe0d833e0815
shreeram a <auteshreeram@gmail.com>
2020-03-07 12:45:48 +0530

    just to check build

--- mycalcy.c
+++ mycalcy.c
@@ -4,13 +4,13 @@ void main(int argc,char* argv[]){
 	float a=0, b=0;
 	if(argc!=3){
 		printf("\n");
-		printf("please provide only 2 numbers to add.");
+		printf("Please provide only 2 numbers to add.");
 		printf("\n");
 		exit(1);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 	printf("\n");
-	printf("addition is %.2f",(a+b));
+	printf("Addition is %.2f",(a+b));
 	printf("\n");
 }
